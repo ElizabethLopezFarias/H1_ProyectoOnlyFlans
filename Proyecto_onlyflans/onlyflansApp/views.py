f0a8de6bca6f1c241b6e4ab9c3aedec286ea93e9
Elizabeth Lopez Farias <160768836+ElizabethLopezFarias@users.noreply.github.com>
2024-05-02 21:24:47 -0400

    Delete Proyecto_onlyflans directory

--- Proyecto_onlyflans/onlyflansApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("""
-        <h1>Bienvenido a OnlyFlans</h1>
-        <p>Los mejores flanes del mundo mundial</p>
-    """)
-# Create your views here.
